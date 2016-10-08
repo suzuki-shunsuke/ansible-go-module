6eb1dce20032583b141b6f08352a6c240f90ba45
Suzuki Shunsuke <suzuki.shunsuke.1989@gmail.com>
2016-10-08 22:27:25 +0900

    Release the version 1.0.0 .

--- README.md
+++ README.md
@@ -1,1 +1,64 @@
 # ansible-go-module
+
+[![Build Status](https://travis-ci.org/suzuki-shunsuke/ansible-go-module.svg?branch=master)](https://travis-ci.org/suzuki-shunsuke/ansible-go-module)
+
+Manage go packages with go command.
+
+## Notice
+
+* This module supports only the "go get" command.
+* This module doesn't support the check mode.
+* If this module succeeds, the result's changed attribute is always true.
+
+## Requirements
+
+* Go
+
+## Install
+
+In the following example this module is installed in ~/ansible/modules directory.
+
+```
+$ mkdir -p ~/ansible/modules
+$ export ANSIBLE_LIBRARY=~/ansible/modules
+$ git clone suzuki-shunsuke/ansible-go-module ~/ansible/modules/ansible-go-module
+```
+
+## Options
+
+parameter | required | default | choices | comments
+--- | --- | --- | --- | ---
+name | yes | | | The name of go package to install
+executable | no | | | The executable path of go command
+gopath | no | | | The environment variable GOPATH
+install | no | yes | bool | If no, the -d option is added to "go get" command
+update | no | no | bool | If yes, the -u option is added to "go get" command
+command | no | get | get | The subcommand of "go" command. Now support only "get"
+
+## Example
+
+```yaml
+# Install hub
+go:
+  name: github.com/github/hub
+
+# Specify GOPATH and the path of go command
+go:
+  name: github.com/github/hub
+  executable: /usr/local/go/bin/go
+  gopath: /home/vagrant/.go
+
+# Not install
+go:
+  name: github.com/github/hub
+  install: no
+
+# Update
+go:
+  name: github.com/github/hub
+  update: yes
+```
+
+## Licence
+
+MIT
---
+++ go.py
@@ -0,0 +1,110 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+DOCUMENTATION = '''
+---
+module: go
+short_description: Install go package.
+options:
+  name:
+    description:
+    - A package name, like (github.com/github/hub)
+    required: true
+  executable:
+    description:
+      - The executable location for go.
+    required: false
+  gopath:
+    description:
+      - The environment variable GOPATH.
+    required: false
+  command:
+    description:
+      - subcommand
+    required: false
+    default: "get"
+  install:
+    description:
+      - When this option is false, it instructs get not to install the package.
+    required: false
+    default: true
+  update:
+    description:
+      - When this option is true, it instructs get to use the network to update the named packages and their dependencies.
+    required: false
+    default: false
+requirements:
+- go
+author: "Suzuki Shunsuke"
+'''
+
+EXAMPLES = '''
+# Install hub
+go:
+  name: github.com/github/hub
+
+# Specify GOPATH and the path of go command
+go:
+  name: github.com/github/hub
+  executable: /usr/local/go/bin/go
+  gopath: /home/vagrant/.go
+
+# Not install
+go:
+  name: github.com/github/hub
+  install: no
+
+# Update
+go:
+  name: github.com/github/hub
+  update: yes
+'''
+
+RETURN = '''
+'''
+
+import json
+
+from ansible.module_utils.basic import AnsibleModule
+
+
+def main():
+    module = AnsibleModule(argument_spec={
+        "name": {"required": True, "type": "str"},
+        "executable": {"required": False, "default": "go", "type": "str"},
+        "gopath": {"required": False, "type": "str"},
+        "install": {"required": False, "default": True, "type": "bool"},
+        "update": {"required": False, "default": False, "type": "bool"},
+        "command": {
+            "required": False, "default": "get",
+            "choices": ["get"]
+        },
+    })
+    # module.run_command_environ_update = APT_ENV_VARS
+    params = module.params
+    go = params["executable"]
+    command = params["command"]
+    package = params["name"]
+    install = params["install"]
+    update = params["update"]
+
+    environ_update = {}
+    if "gopath" in params:
+        environ_update["GOPATH"] = params["gopath"]
+
+    cmd = [go, command]
+    if not install:
+        cmd.append("-d")
+    if update:
+        cmd.append("-u")
+    cmd.append(package)
+
+    rc, out, err = module.run_command(cmd, environ_update=environ_update)
+    if rc:
+        module.fail_json(msg=err, stdout=out)
+    else:
+        module.exit_json(changed=True, stdout=out, stderr=err)
+
+
+if __name__ == '__main__':
+    main()
